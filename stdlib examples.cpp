7e221e586e7ff59181b6e34cd4b8e505c86e8fa2
PJBoy <patrick.johnston.2012@my.bristol.ac.uk>
2017-03-07 13:51:44 +0000

    Updated regex example

--- stdlib examples.cpp
+++ stdlib examples.cpp
@@ -147,8 +147,13 @@ void ll_iterator_example()
 void regex()
 {
 	const std::regex pattern("defi(?:n(?:ate|[ae]n?t)|ant)ly", std::regex::icase | std::regex::optimize);
-	for (std::string line; std::getline(std::cin, line);)
+	for (;;)
 	{
+		std::cout << "Spell definitely\n";
+		std::string line;
+		std::getline(std::cin, line);
+
+		try
 		{
 			if (std::regex_match(line, pattern))
 				std::cout << "That was a known mispelling of definitely\n";
@@ -163,7 +168,12 @@ void regex()
 
 			std::cout << "Match 0 is: " << match[0] << '\n';
 		}
+		catch (const std::regex_error& e)
+		{
+			std::cerr << "Regex error in match example\n";
+		}
 
+		try
 		{
 			if (std::regex_search(line, pattern))
 				std::cout << "That contained a known mispelling of definitely\n";
@@ -177,11 +187,35 @@ void regex()
 
 			std::cout << "Match 0 is: " << match[0] << '\n';
 		}
+		catch (const std::regex_error& e)
+		{
+			std::cerr << "Regex error in search example\n";
+		}
 
+		try
 		{
 			std::cout << "These are the known mispellings of definitely that occurred:\n";
 			for (std::sregex_iterator it_match(std::begin(line), std::end(line), pattern), it_match_end{}; it_match != it_match_end; ++it_match)
 				std::cout << '\t' << (*it_match)[0] << '\n';
+		}
+		catch (const std::regex_error& e)
+		{
+			std::cerr << "Regex error in regex_iterator example\n";
+		}
+
+		try
+		{
+			std::cout << "Here is the correct version:\n";
+			std::cout << std::regex_replace(line, pattern, "definitely") << '\n';
+
+			std::cout << "Enter two words and they will be swapped:\n";
+			std::string words;
+			std::getline(std::cin, words);
+			std::cout << std::regex_replace(words, std::regex("(.+) (.+)"), "$2 $1\n");
+		}
+		catch (const std::regex_error& e)
+		{
+			std::cerr << "Regex error in replace example\n";
 		}
 	}
 }
